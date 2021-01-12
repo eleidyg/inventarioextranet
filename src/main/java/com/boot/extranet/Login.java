e44dd4446fd837c78415363b9df1702911b94ffa
leidy.gama <ednagama4@gmail.com>
2021-01-11 21:35:54 -0500

    Desplieue Cross Origin

--- src/main/java/com/boot/extranet/Login.java
+++ src/main/java/com/boot/extranet/Login.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class Login {
 	
    @RequestMapping(method = RequestMethod.GET, path = "/login/{nombre}")
-   @CrossOrigin
    public String Validación(@PathVariable String nombre) {
 	   if(nombre.equals("edna")) {
 		   return "Ingreso";
